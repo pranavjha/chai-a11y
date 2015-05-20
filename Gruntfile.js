68a801412202f54e6509b28403b380cfc9f0f4d9
Pranav Shekhar Jha <jha.pranav.s@gmail.com>
2015-05-19 20:33:18 -0700

    added tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,145 +37,170 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    clean: [
-      '.coverage',
-      '.test',
-      '.cache'
-    ],
-    jshint: {
-      lib: {
-        src: [
-          'lib/**/*.js',
-          'Gruntfile.js',
-          'package.json'
-        ],
-        options: jshintOptions
-      }
-    },
-    karma: {
-      testAMD: {
-        reporters: [
-          'mocha'
-        ],
-        frameworks: [
-          'requirejs',
-          'mocha',
-          'chai'
-        ],
-        files: [
-          {
-            src: [
-              'node_modules/accessibility-developer-tools/dist/js/axs_testing.js',
-              'node_modules/requirejs/require.js',
-              'test/require-config.js'
-            ],
-            served: true,
-            included: true
+      pkg: grunt.file.readJSON('package.json'),
+      clean: [
+        '.coverage',
+        '.test',
+        '.cache'
+      ],
+      jshint: {
+        lib: {
+          src: [
+            'lib/**/*.js',
+            'Gruntfile.js',
+            'package.json'
+          ],
+          options: jshintOptions
+        }
+      },
+      karma: {
+        testAMD: {
+          frameworks: [
+            'requirejs',
+            'mocha',
+            'chai'
+          ],
+          files: [
+            {
+              src: [
+                'node_modules/accessibility-developer-tools/dist/js/axs_testing.js',
+                'node_modules/requirejs/require.js',
+                'test/require-config.js'
+              ],
+              served: true,
+              included: true
+            },
+            {
+              src: [
+                'test/amd-specs.js',
+                'lib/**/*.js',
+                'node_modules/jquery/dist/jquery.js'
+              ],
+              served: true,
+              included: false
+            }
+          ],
+          plugins: [
+            'karma-requirejs',
+            'karma-chai',
+            'karma-mocha',
+            'karma-mocha-reporter',
+            'karma-phantomjs-launcher'
+          ]
+        },
+        testCommonJs: {
+          frameworks: [
+            'optimizer',
+            'mocha'
+          ],
+          optimizer: {
+            minify: false,
+            bundlingEnabled: false,
+            cacheProfile: 'development',
+            tempdir: '.test',
+            ignore: 'node_modules/accessibility-developer-tools/**'
           },
-          {
-            src: [
-              'test/amd-specs.js',
-              'lib/**/*.js',
-              'node_modules/jquery/dist/jquery.js'
-            ],
-            served: true,
-            included: false
-          }
-        ],
-        plugins: [
-          'karma-requirejs',
-          'karma-chai',
-          'karma-mocha',
-          'karma-mocha-reporter',
-          'karma-phantomjs-launcher'
-        ]
-      },
-      testCommonJs: {
-        reporters: [
-          'mocha'
-        ],
-        frameworks: [
-          'optimizer',
-          'mocha'
-        ],
-        optimizer: {
-          minify: false,
-          bundlingEnabled: false,
-          cacheProfile: 'development',
-          tempdir: '.test',
-          ignore: 'node_modules/accessibility-developer-tools/**'
+          files: [
+            {
+              src: [
+                'test/commonjs-specs.js',
+                'node_modules/accessibility-developer-tools/dist/js/axs_testing.js'
+              ]
+            }
+          ],
+          plugins: [
+            'karma-chai',
+            'karma-mocha',
+            'karma-optimizer',
+            'karma-mocha-reporter',
+            'karma-phantomjs-launcher'
+          ]
         },
-        files: [
-          {
-            src: [
-              'test/commonjs-specs.js',
-              'node_modules/accessibility-developer-tools/dist/js/axs_testing.js'
-            ]
-          }
-        ],
-        plugins: [
-          'karma-chai',
-          'karma-mocha',
-          'karma-optimizer',
-          'karma-mocha-reporter',
-          'karma-phantomjs-launcher'
-        ]
-      },
-      options: {
-        browsers: [
-          'PhantomJS'
-        ],
-        client: {
-          mocha: {
-            timeout: 500,
-            ui: 'bdd'
+        testScriptTag: {
+          frameworks: [
+            'mocha',
+            'chai'
+          ],
+          files: [
+            {
+              src: [
+                'node_modules/jquery/dist/jquery.js',
+                'node_modules/accessibility-developer-tools/dist/js/axs_testing.js',
+                'lib/index.js',
+                'test/script-tag-specs.js'
+              ]
+            }
+          ],
+          plugins: [
+            'karma-chai',
+            'karma-mocha',
+            'karma-mocha-reporter',
+            'karma-phantomjs-launcher'
+          ],
+          client: {
+            mocha: {
+              ui: 'bdd'
+            }
           }
         },
-        singleRun: true
-      }
-    },
-    // Configure a mochaTest task
-    mochaTest: {
-      test: {
         options: {
-          reporter: 'spec'
-        },
-        src: [
-          'test/nodejs-specs.js'
-        ]
-      }
-    },
-    docco: {
-      debug: {
-        src: [
-          'lib/**',
-          'README.md'
-        ],
-        options: {
-          output: '.docs/'
-        }
-      }
-    },
-    'gh-pages': {
-      options: {
-        base: '.docs',
-        // GH_TOKEN is the environment variable holding the access token for the repository
-        repo: 'https://' + process.env.GH_TOKEN + '@github.com/pranavjha/chai-a11y.git',
-        clone: '.gh_pages',
-        message: 'auto commit chai-a11y on <%= grunt.template.today("yyyy-mm-dd") %>',
-        // This configuration will suppress logging and sanitize error messages.
-        silent: true,
-        user: {
-          name: 'Pranav Jha',
-          email: 'jha.pranav.s@gmail.com'
+          browsers: [
+            'PhantomJS'
+          ],
+          client: {
+            mocha: {
+              timeout: 500,
+              ui: 'bdd'
+            }
+          },
+          singleRun: true,
+          reporters: [
+            'mocha'
+          ]
         }
       },
-      src: [
-        '**'
-      ]
+      // Configure a mochaTest task
+      mochaTest: {
+        test: {
+          options: {
+            reporter: 'spec'
+          },
+          src: [
+            'test/nodejs-specs.js'
+          ]
+        }
+      },
+      docco: {
+        debug: {
+          src: [
+            'lib/**',
+            'README.md'
+          ],
+          options: {
+            output: '.docs/'
+          }
+        }
+      },
+      'gh-pages': {
+        options: {
+          base: '.docs',
+          // GH_TOKEN is the environment variable holding the access token for the repository
+          repo: 'https://' + process.env.GH_TOKEN + '@github.com/pranavjha/chai-a11y.git',
+          clone: '.gh_pages',
+          message: 'auto commit chai-a11y on <%= grunt.template.today("yyyy-mm-dd") %>',
+          // This configuration will suppress logging and sanitize error messages.
+          silent: true,
+          user: {
+            name: 'Pranav Jha',
+            email: 'jha.pranav.s@gmail.com'
+          }
+        },
+        src: [
+          '**'
+        ]
+      }
     }
-  });
+  );
   grunt.registerTask('test', [
     'jshint',
     'mochaTest',
