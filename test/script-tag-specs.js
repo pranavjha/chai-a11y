8005860fa4a6aa522f836ee986f9cb3b83a14cd3
Damodaran Sathyakumar <dasathyakuma@ebay.com>
2016-02-16 18:36:57 +0530

    Integrated with version 0.0.4 of a11y-auditor
    Fixed test cases AMD and script tag. Changed normalize-config. Removed
    the result from stringily-response. Updated version of a11y-auditor in
    package.json. Fixed a few comments & lines of code for joshing
    purposes. As of now common js and node js test cases are failing. Need
    to check.

--- test/script-tag-specs.js
+++ test/script-tag-specs.js
@@ -2,25 +2,29 @@
 describe('chai a11y', function() {
     'use strict';
     it('should be able to validate an html string', function() {
-        return expect('<button>Abacus</button>').to.be.accessible();
+        return expect('<button>Abacus</button>').to.be.accessible({
+          ignore : ['AX_23', 'AX_04', 'AX_05', 'AX_32']
+        });
     });
     it('should be able to ignore one rule', function() {
         return expect('<button id="btn_1"></button>').to.be.accessible({
-            "#btn_1": ["AX_23"]
+          ignore : ['AX_23', 'AX_04', 'AX_05', 'AX_32']
         });
     });
     it('should be able to ignore stuff with rulename', function() {
         return expect('<button></button>').to.be.accessible({
-            "button": ["AX_23", "AX_04", "AX_05", "AX_32"]
+          ignore : ['AX_23', 'AX_04', 'AX_05', 'AX_32']
         });
     });
     it('should be able to ignore stuff using selectors', function() {
         return expect('<button id="btn_1"></button>').to.be.accessible({
-            "#btn_1": ["AX_23", "AX_04", "AX_05", "AX_32"]
+          ignore : ['AX_23', 'AX_04', 'AX_05', 'AX_32']
         });
     });
     it('should not pollute the DOM', function() {
-        return expect('<button id="temp_id">temp</button>').to.be.accessible().then(function() {
+        return expect('<button id="temp_id">temp</button>').to.be.accessible({
+          ignore : ['AX_23', 'AX_04', 'AX_05', 'AX_32']
+        }).then(function() {
             return expect($('#temp_id').size()).to.eql(0);
         });
     });
@@ -28,7 +32,7 @@ describe('chai a11y', function() {
         var btn = $('<button id="temp_id">temp</button>');
         $('body').append($('<div id="temp_div"></div>').append(btn));
         return expect(btn).to.be.accessible({
-            "#temp_id": ["AX_23", "AX_04", "AX_05", "AX_32"]
+          ignore : ['AX_23', 'AX_04', 'AX_05', 'AX_32']
         }).then(function() {
             expect($('#temp_div > #temp_id').size()).to.eql(1);
             $('#temp_id').remove();
